7e60b936af918c67405abcb1b0d5605879fbb644
Pedro Martinez <pmartinez752@unab.edu.co>
2023-10-05 05:09:34 -0500

    Vista medico con Style

--- medico.php
+++ medico.php
@@ -21,24 +21,47 @@
             </div>
         </li>
     </ul>
+    
     <br>
-    <form>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <br>
+    <div class="container-text-center">
+        <div class="row">
+            <div class="col">
+                Paciente
+                <br>
+                <br>
+                Pedrito Martinez
+                <?php
+                
+                ?>
+
+            </div>
+            <div class="col">
+                Turno
+                <br>
+                <br>
+
+                A01
+
+            </div>
+            <div class="col">
+                Llamar 
+                <br>
+                <br>
+                <button type="button" class="btn btn-success">Llamar</button>
+            </div>
+
+            <div  class= "col">
+                Quitar 
+                <br>
+                <br>
+                <button type="button" class="btn btn-danger">Quitar</button>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-    <div class="square"></div>
+    </div>
+
+    
+    <img class="soporteimagen" src="img/soporte.png"  alt="soporteimagen" >
 
 </body>
 
